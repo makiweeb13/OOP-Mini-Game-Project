93ad68fbdb2b44de1a701642365e3a5e202de458
UsernameNI <22103894@usc.edu.ph>
2024-04-21 00:43:01 +0800

    BACKGROUND

--- src/Game.java
+++ src/Game.java
@@ -6,14 +6,13 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable{
     private Thread thread;
     private boolean running = false;
     private BufferedImage backgroundImage;
 
     public Game(){
         try{
-            // Load the background image
             backgroundImage = ImageIO.read(new File("TEST/practice.jpg"));
         }catch(IOException e){
             e.printStackTrace();
@@ -21,13 +20,13 @@ public class Game extends Canvas implements Runnable {
         new Window("Pokemon Game", this);
     }
 
-    public synchronized void start() {
+    public synchronized void start(){
         thread = new Thread(this);
         thread.start();
         running = true;
     }
 
-    public synchronized void stop() {
+    public synchronized void stop(){
         try {
             thread.join();
             running = false;
@@ -36,8 +35,7 @@ public class Game extends Canvas implements Runnable {
         }
     }
 
-    // Implementing game loop
-    public void run() {
+    public void run(){
         long lastTime = System.nanoTime();
         double amountOfTicks = 60.0;
         double ns = 1000000000 / amountOfTicks;
@@ -56,7 +54,7 @@ public class Game extends Canvas implements Runnable {
                 render();
             frames++;
 
-            if (System.currentTimeMillis() - timer > 1000) {
+            if (System.currentTimeMillis() - timer > 1000){
                 timer += 1000;
                 System.out.println("FPS " + frames);
                 frames = 0;
@@ -64,28 +62,20 @@ public class Game extends Canvas implements Runnable {
         }
         stop();
     }
-
-    private void tick() {
-        // Game logic update
+    private void tick(){
     }
-
-    private void render() {
+    private void render(){
         BufferStrategy bs = this.getBufferStrategy();
-        if (bs == null) {
-            this.createBufferStrategy(3); // creates 3 buffers within the game
+        if (bs == null){
+            this.createBufferStrategy(3);
             return;
         }
         Graphics g = bs.getDrawGraphics();
-
-        // Draw the background image
         g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
-
-        // Release resources
         g.dispose();
         bs.show();
     }
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Start");
         new Game();
     }
