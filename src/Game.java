26d34029ad705a7613aba2e0d67f1877e8e70aae
18100807 <18100807@usc.edu.ph>
2024-04-29 04:38:50 +0800

    blinking - startImage

--- src/Game.java
+++ src/Game.java
@@ -11,7 +11,10 @@ public class Game extends Canvas implements Runnable {
     private boolean running = false;
     private Render render;
     private BufferedImage backgroundImage;
+    private BufferedImage backgroundImage2;
     private BufferedImage logoImage;
+    private BufferedImage startImage;
+    private boolean showStartImage = true;
     protected int gameState;
     protected final int titleState = 0;
     protected final int selectionState = 1;
@@ -78,7 +81,9 @@ public class Game extends Canvas implements Runnable {
     private void render(){
         try {
             backgroundImage = ImageIO.read(new File("resources/images/background1.jpg"));
+            backgroundImage2 = ImageIO.read(new File("resources/images/background2.jpg"));
             logoImage = ImageIO.read(new File("resources/images/Logo.png"));
+            startImage = ImageIO.read(new File("resources/images/Start.png"));
         } catch(IOException e) {
             e.printStackTrace();
         }
@@ -92,27 +97,34 @@ public class Game extends Canvas implements Runnable {
 
         if (gameState == titleState) {
             g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
+
         /*
         "Don't remove these comments so that others will know how this code works."
          */
-            try{ //is the code for creating the button "Press 'ENTER' to start the game".
-                BufferedImage startImage = ImageIO.read(new File("resources/images/Start.png"));
+            if (showStartImage) { // Only draw the start image if showStartImage is true
                 int startWidth = startImage.getWidth();
                 int startHeight = startImage.getHeight();
-                //this code is to calculate the position of the image horizontally and vertically.
                 int startX = getWidth() / 2 - startWidth / 2;
                 int startY = getHeight() / 2 + ((getHeight() / 2) - startHeight);
                 g.drawImage(startImage, startX, startY, startWidth, startHeight, null);
-            } catch(IOException e){
-                e.printStackTrace();
             }
+
         } else if (gameState == selectionState) {
-            try {
-                backgroundImage = ImageIO.read(new File("resources/images/background2.jpg"));
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
-            g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), null);
+            g.drawImage(backgroundImage2, 0, 0, getWidth(), getHeight(), null);
+
+            Font font = new Font("Arial", Font.BOLD, 20);
+            g.setFont(font);
+            g.setColor(Color.BLACK);
+            String text = "Choose your";
+            int textX = ((getWidth() - g.getFontMetrics().stringWidth(text)) / 2) - 90;
+            int textY = 50;
+
+            g.drawString(text, textX, textY);
+
+            int logoX = textX + g.getFontMetrics().stringWidth(text) + 10; // Add some padding between the string and the logo
+            int logoY = 0;
+            g.drawImage(logoImage, logoX, logoY, null);
+
         } else if (gameState == battleState) {
             g.fillRect(0, 0, getWidth(), getHeight());
             g.setColor(Color.BLUE);
@@ -121,6 +133,14 @@ public class Game extends Canvas implements Runnable {
         render.render(g);
         bs.show();
         g.dispose();
+
+        // Toggle the visibility of the start image every half second
+        try {
+            Thread.sleep(500); // Sleep for 500 milliseconds (half second)
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        showStartImage = !showStartImage; // Toggle the visibility
     }
 
     public static void main(String[] args){
